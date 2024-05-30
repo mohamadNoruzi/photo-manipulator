879cd13c482bf0b86ed1bc610a339812e9123dd3
mohammadNoruzi <mohammadnorozy77@gmail.com>
2024-05-30 21:37:31 +0330

    a

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,14 @@ import React from "react";
 
 export default function index() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>index</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    color: "red",
+  },
+});
