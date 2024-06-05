53d714a1c40102f1117e1d22d35bd709c96e1f88
mohammadNoruzi <mohammadnorozy77@gmail.com>
2024-06-05 22:38:34 +0330

    animated toggle

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -6,7 +6,7 @@ import {
   TouchableWithoutFeedback,
   View,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Animated, {
   useAnimatedStyle,
@@ -16,17 +16,26 @@ import Animated, {
 } from "react-native-reanimated";
 
 const CustomHeader = () => {
-  const alItems = useSharedValue<"flex-start" | "flex-end" | "center">(
-    "flex-start"
-  );
+  const [status, setStatus] = useState(false);
+  const backgroundColor = useSharedValue("red");
+  const paddingLeft = useSharedValue(0);
+
   const animatedStyle = useAnimatedStyle(() => {
-    return { alignItems: alItems.value };
+    console.log("paddingLeft", paddingLeft.value);
+    return {
+      paddingLeft: paddingLeft.value,
+      backgroundColor: backgroundColor.value,
+    };
   });
 
   const moveToggle = () => {
-    alItems.value = withDelay(1, withTiming("flex-end", { duration: 2000 }));
-    // alItems.value = "flex-end";
-    console.log("alItems", alItems.value);
+    let padding = status ? 0 : 50;
+    paddingLeft.value = withDelay(0.1, withTiming(padding, { duration: 500 }));
+    backgroundColor.value = withDelay(
+      1,
+      withTiming("yellow", { duration: 2000 })
+    );
+    setStatus(!status);
   };
 
   return (
@@ -54,8 +63,8 @@ const styles = StyleSheet.create({
     marginLeft: 30,
     backgroundColor: "red",
     borderRadius: 40,
-    // paddingLeft: 50,
-    alignItems: "flex-start",
+    paddingLeft: 0,
+    // alignItems: "flex-start",
   },
   circle: {
     width: 40,
